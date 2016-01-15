b3e52ffce7c6242861195531e6ce0273467375f7
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-14 23:38:56 -0500

    removed repeated console.logs and replaces with repeat new line commands

--- liri.js
+++ liri.js
@@ -29,39 +29,37 @@ switch(params[0]) {
     break;
   }
 
-
-
 function spotifyCall (arg) {
-
   spotify.search({ type: 'track', query: arg }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
     var albumInfo = data.tracks.items[0];
-    console.log("Artist: " + albumInfo.artists[0].name);
-    console.log("Track Name: " + albumInfo.name);
-    console.log("Preview Link: " + albumInfo.preview_url);
-    console.log("Album: " + albumInfo.album.name);
+    
+
+    console.log("Artist: " + albumInfo.artists[0].name + "\r\n" +
+                "Track Name: " + albumInfo.name + "\r\n" + 
+                "Preview Link: " + albumInfo.preview_url + "\r\n" +
+                "Album: " + albumInfo.album.name);
   });
 }
 
-
-function movieCall (arg) {
+function movieCall () {
   var omdbApi = 'http://www.omdbapi.com/?t=';
   var query = params[1];
   var jsonEnd = '&y=&plot=short&r=json&tomatoes=true';
   
   request(omdbApi+query+jsonEnd, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log("Title: "+ JSON.parse(body)["Title"]);
-      console.log("Year: "+ JSON.parse(body)["Year"]);
-      console.log("IMDB Rating: "+ JSON.parse(body)["imdbRating"]);
-      console.log("Country: "+ JSON.parse(body)["Country"]);
-      console.log("Language: "+ JSON.parse(body)["Language"]);
-      console.log("Plot: "+ JSON.parse(body)["Plot"]);
-      console.log("Actors: "+ JSON.parse(body)["Actors"]);
-      console.log("Rotten Tomatoes Rating: "+ JSON.parse(body)["tomatoRating"]);
+      console.log("Title: "+ JSON.parse(body)["Title"]+ "\r\n" +
+                  "Year: "+ JSON.parse(body)["Year"]+ "\r\n" +
+                  "IMDB Rating: "+ JSON.parse(body)["imdbRating"]+ "\r\n" +
+                  "Country: "+ JSON.parse(body)["Country"]+ "\r\n" +
+                  "Language: "+ JSON.parse(body)["Language"]+ "\r\n" +
+                  "Plot: "+ JSON.parse(body)["Plot"]+ "\r\n" +
+                  "Actors: "+ JSON.parse(body)["Actors"]+ "\r\n" +
+                  "Rotten Tomatoes Rating: "+ JSON.parse(body)["tomatoRating"]);
     }
   });
 }
